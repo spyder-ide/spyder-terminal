1b586fca31c08ff367a891cf06e7d68bf32d37f2
Carlos Cordoba <ccordoba12@gmail.com>
2017-04-03 00:36:30 -0500

    Testing: Clear terminal before getting pwd

--- spyder_terminal/tests/test_terminal.py
+++ spyder_terminal/tests/test_terminal.py
@@ -52,10 +52,14 @@ def test_new_terminal(qtbot):
     # Setup widget
     terminal = setup_terminal(qtbot)
     term = terminal.get_current_term()
-    qtbot.wait(1000)
+    qtbot.wait(TERM_UP)
 
     # Move to LOCATION
     qtbot.keyClicks(term.view, 'cd {}'.format(LOCATION))
+    qtbot.keyPress(term.view, Qt.Key_Return)
+
+    # Clear
+    qtbot.keyClicks(term.view, 'clear')
     qtbot.keyPress(term.view, Qt.Key_Return)
 
     # Run pwd
