212e0aa4ac0bb5779dd3a6ff1e14795306cd7eff
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-04-11 16:44:54 -0500

    Server port is now variable

--- spyder_terminal/server/main.py
+++ spyder_terminal/server/main.py
@@ -5,11 +5,19 @@
 import os
 import routes
 import logging
+import argparse
 import coloredlogs
 import tornado.web
 import tornado.ioloop
 from logic import term_manager
 
+
+parser = argparse.ArgumentParser(
+    description='Websocket-based bash terminal server')
+
+parser.add_argument('--port',
+                    default=8070,
+                    help="TCP port to be listened on")
 
 LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
               '-35s %(lineno) -5d: %(message)s')
@@ -21,7 +29,7 @@ if os.name == 'nt':
     clr = 'cls'
 
 
-def main():
+def main(port):
     """Create and setup a new tornado server."""
     logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
     settings = {"static_path": os.path.join(
@@ -30,11 +38,11 @@ def main():
                                           debug=True,
                                           serve_traceback=True,
                                           autoreload=True, **settings)
-    print("Server is now at: 127.0.0.1:8070")
+    print("Server is now at: 127.0.0.1:{}".format(port))
     application.term_manager = term_manager.TermManager()
     application.logger = LOGGER
     ioloop = tornado.ioloop.IOLoop.instance()
-    application.listen(8070, address='127.0.0.1')
+    application.listen(port, address='127.0.0.1')
     try:
         ioloop.start()
     except KeyboardInterrupt:
@@ -46,4 +54,6 @@ def main():
 
 if __name__ == '__main__':
     os.system(clr)
-    main()
+    args = parser.parse_args()
+    port = int(args.port)
+    main(port)
