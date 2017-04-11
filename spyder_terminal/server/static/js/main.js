096d593aee5d9e555af838c17dd1a443f16828c5
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-04-11 00:10:23 -0500

    Terminal does load locally installed fonts

--- spyder_terminal/server/static/js/main.js
+++ spyder_terminal/server/static/js/main.js
@@ -56,6 +56,7 @@ function createTerminal() {
   term.open(terminalContainer);
   term.fit();
   term.toggleFullscreen(true);
+  console.log($('.terminal').css('font-family'));
 
   var initialGeometry = term.proposeGeometry(),
       cols = initialGeometry.cols,
