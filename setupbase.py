04547cdeddede0bdf097798bec92eb989834a5a9
Carlos Cordoba <ccordoba12@gmail.com>
2017-08-24 21:21:56 -0500

    Setupbase: Add a copyright header

--- setupbase.py
+++ setupbase.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) Spyder Project Contributors
+# Copyright (c) 2015-, Jupyter Development Team.
+# Copyright (c) 2008-2015, IPython Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# -----------------------------------------------------------------------------
 
 """
 General setup rules to download external JS dependencies
