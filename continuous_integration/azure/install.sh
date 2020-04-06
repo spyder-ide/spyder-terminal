0fa7a14931fa968e68c3d08bf8da2f4cc98b203f
Stephannie Jimenez <steff456@hotmail.com>
2020-04-06 17:41:34 -0500

    install nodejs latest version on the CI

--- continuous_integration/azure/install.sh
+++ continuous_integration/azure/install.sh
@@ -10,9 +10,6 @@ if [ $(uname) == Darwin ]; then
     conda install -q -y qt=5.9.6
 fi
 
-# Install nodejs
-conda install -q -y -c conda-forge nodejs 
-
 # Install yarn
 conda install -q -y -c conda-forge yarn
 
