649b9af2c9b2bb979d6c1297704efe768434b278
Stephannie Jimenez <steff456@hotmail.com>
2020-04-06 18:09:39 -0500

    add path to nvm

--- continuous_integration/circle/runtests.sh
+++ continuous_integration/circle/runtests.sh
@@ -4,6 +4,8 @@ set -ex
 source $HOME/miniconda/etc/profile.d/conda.sh
 conda activate test
 
+export NVM_DIR="/opt/circleci/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use v13.10.1
 
 python setup.py build_static
