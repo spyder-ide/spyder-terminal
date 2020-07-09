cb15663c364867c7d475cb9cce9361406632755e
Stephannie Jimenez <steff456@hotmail.com>
2020-07-09 18:34:09 -0500

    Percent encode/decode cwd to encode spaces and special characters

--- spyder_terminal/server/web/main_handler.py
+++ spyder_terminal/server/web/main_handler.py
@@ -1,10 +1,10 @@
 # -*- coding: iso-8859-15 -*-
 
 """Basic static index.html HTTP handler."""
-
 import tornado.web
 import tornado.escape
 from os import getcwd
+from urllib.parse import quote
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -18,7 +18,9 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         """Get static index.html page."""
         cwd = self.get_argument('path', getcwd())
-        self.set_cookie('cwd', cwd)
+        # We need to do percent encoding for sending the info through a cookie
+        # For further information see spyder-ide/spyder-terminal#225
+        self.set_cookie('cwd', quote(cwd))
         self.render('../static/build/index.html')
 
     @tornado.gen.coroutine
