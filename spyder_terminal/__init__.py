b2127227ed22a5e2d7ce58997fdf4356c0634f5e
Stephannie Jimenez <steff456@hotmail.com>
2020-07-21 15:43:28 -0500

    Back to work

--- spyder_terminal/__init__.py
+++ spyder_terminal/__init__.py
@@ -11,5 +11,5 @@ from .terminalplugin import TerminalPlugin as PLUGIN_CLASS
 
 PLUGIN_CLASS
 
-VERSION_INFO = (0, 4, 0)
+VERSION_INFO = (0, 5, 0, 'dev0')
 __version__ = '.'.join(map(str, VERSION_INFO))
