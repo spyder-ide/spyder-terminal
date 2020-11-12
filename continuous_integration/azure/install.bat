6dcd24c68c52048e8938e9ddec0ea845cfcf207e
Stephannie Jimenez <steff456@hotmail.com>
2020-11-12 18:32:59 -0500

    upgrade windows node version

--- continuous_integration/azure/install.bat
+++ continuous_integration/azure/install.bat
@@ -1,7 +1,3 @@
-:: npm using https for git
-git config --global url."https://github.com/".insteadOf git@github.com:
-git config --global url."https://".insteadOf git://
-
 :: Install dependencies
 if %USE_CONDA% == yes (
   if %PYTHON_VERSION% == 3.6 (
