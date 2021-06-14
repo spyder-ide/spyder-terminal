7b42d4abbd72e061ba0b016cd61f05654ff35c15
Stephannie Jimenez <steff456@hotmail.com>
2021-06-14 15:06:14 -0500

    change test installation channel

--- continuous_integration/azure/install.sh
+++ continuous_integration/azure/install.sh
@@ -4,7 +4,7 @@
 conda install -q -y -c conda-forge --file requirements/conda.txt
 
 # Install test dependencies
-conda install -q -y -c conda-forge --file requirements/tests.txt
+conda install -q -y -c spyder-ide --file requirements/tests.txt
 
 conda install -q -y -c conda-forge nodejs=13
 
