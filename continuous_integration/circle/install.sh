ed005cbc13b27adb4ec876c9c1050539729a1164
Stephannie Jimenez <steff456@hotmail.com>
2022-03-28 11:53:08 +0200

    Add -y flag

--- continuous_integration/circle/install.sh
+++ continuous_integration/circle/install.sh
@@ -30,7 +30,7 @@ nvm install v13.10.1
 # -- Install Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+sudo apt-get update && sudo apt-get -y install yarn
 export PATH="$PATH:/opt/yarn-1.17.3/bin"
 
 pip install -q codecov
