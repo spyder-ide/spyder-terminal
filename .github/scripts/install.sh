ec387cba66f783d57e7677fc80b47c833be2c949
Stephannie Jimenez <steff456@hotmail.com>
2022-03-29 00:25:14 +0200

    Migrate windows and mac

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -ex
 
-# Install dependencies
-mamba install -q -y -c conda-forge --file requirements/conda.txt
+if [[ $OS == 'win']]; then
+    mamba install -q -y -c conda-forge --file requirements/conda_win.txt
+else
+    # Install dependencies
+    mamba install -q -y -c conda-forge --file requirements/conda.txt
+fi
+
 
 # Install test dependencies
 mamba install -q -y -c conda-forge --file requirements/tests.txt
