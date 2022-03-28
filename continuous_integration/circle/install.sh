9eccf82dd51439b0e4a76e9ce717851fdb849c84
Stephannie Jimenez <steff456@hotmail.com>
2022-03-28 11:41:26 +0200

    Install nvm

--- continuous_integration/circle/install.sh
+++ continuous_integration/circle/install.sh
@@ -16,6 +16,8 @@ conda install -q -y -c conda-forge --file requirements/conda.txt
 # Install test dependencies
 conda install -q -y -c conda-forge --file requirements/tests.txt
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
 # Update to latest version of nodejs using nvm
 export NVM_DIR="/opt/circleci/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
