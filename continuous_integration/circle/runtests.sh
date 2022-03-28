87d25b83a434dad35a32ac430ce8b43bcdb32644
Stephannie Jimenez <steff456@hotmail.com>
2022-03-28 12:01:09 +0200

    Add fix in runtests

--- continuous_integration/circle/runtests.sh
+++ continuous_integration/circle/runtests.sh
@@ -4,8 +4,9 @@ set -ex
 source $HOME/miniconda/etc/profile.d/conda.sh
 conda activate test
 
-export NVM_DIR="/opt/circleci/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 nvm use v13.10.1
 
 python setup.py build_static
