a32fba8f144dc2f26959773c2ee132aa0f90789e
FMasson <97910143+Zybulon@users.noreply.github.com>
2024-09-07 16:17:34 +0200

    add_corner_widget interface evolution

--- spyder_terminal/widgets/main_widget.py
+++ spyder_terminal/widgets/main_widget.py
@@ -166,9 +166,7 @@ class TerminalMainWidget(PluginMainWidget):
             triggered=lambda: self.create_new_term(),
         )
 
-        self.add_corner_widget(
-            TerminalMainWidgetCornerToolbar.NewTerm,
-            new_terminal_toolbar_action)
+        self.add_corner_widget(new_terminal_toolbar_action)
 
         new_terminal_cwd = self.create_action(
             TerminalMainWidgetActions.NewTerminalForCWD,
