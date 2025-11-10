0ccab4fb600ef7079dc862f4b4b0a4206e8fae2e
dalthviz <16781833+dalthviz@users.noreply.github.com>
2025-11-10 16:54:25 -0500

    Testing

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ex
 
 # Install dependencies
-if [ "$OS" == "win" ]; then
-    mamba install -q -y -c conda-forge --file "requirements/conda_win.txt"
+if [ "$OS" = "win" ]; then
+    mamba install -q -y -c conda-forge --file requirements/windows.txt
 else
-    mamba install -q -y -c conda-forge --file requirements/conda.txt
+    mamba install -q -y -c conda-forge --file requirements/unix.txt
 fi
 
 
