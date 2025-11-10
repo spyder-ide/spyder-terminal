472836924f55a48cfca232c98d03db9b10c0433f
dalthviz <16781833+dalthviz@users.noreply.github.com>
2025-11-10 17:09:01 -0500

    Testing

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -2,13 +2,13 @@
 
 # Install dependencies
 if [ "$OS" = "win" ]; then
-    mamba install -q -y -c conda-forge --file requirements/windows.txt
+    conda install -q -y -c conda-forge --file requirements/windows.txt
 else
-    mamba install -q -y -c conda-forge --file requirements/unix.txt
+    conda install -q -y -c conda-forge --file requirements/unix.txt
 fi
 
 
 # Install test dependencies
-mamba install -q -y -c conda-forge --file requirements/tests.txt
+conda install -q -y -c conda-forge --file requirements/tests.txt
 
-mamba install -q codecov
+conda install -q codecov
